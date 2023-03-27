a9533655d5b988db37fcbaf962ed6d7e8b8e33f6
etozhealexis <alexisworksya@yandex.ru>
2023-03-27 21:24:12 +0300

    2.2.1

---
+++ src/main/java/hiber/dao/UserDao.java
@@ -0,0 +1,16 @@
+package hiber.dao;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import hiber.model.Car;
+import hiber.model.User;
+
+import java.util.List;
+
+public interface UserDao {
+   void add(User user);
+   List<User> listUsers();
+
+   User getUserByCar(Car car);
+}
